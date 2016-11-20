fcfc6c5dc16fccaeab69fd7bc27706d33dd5661e
Ilya Glazunov <ilyaglazunov@Vladislavs-Mac-mini-2.local>
2016-11-20 17:07:09 +0400

    123

---
+++ ssau-chatTests/ssau_chatTests.m
@@ -0,0 +1,39 @@
+//
+//  ssau_chatTests.m
+//  ssau-chatTests
+//
+//  Created by Ilya Glazunov on 20.11.16.
+//  Copyright © 2016 Ilya Glazunov. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ssau_chatTests : XCTestCase
+
+@end
+
+@implementation ssau_chatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
